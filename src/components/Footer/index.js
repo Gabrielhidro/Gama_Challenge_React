0cdb6bf9346b2b0e38ee268e7c02e18a1d36e497
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-13 20:10:30 -0300

    Add styles to Footer and social links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,11 +7,11 @@ const Footer = () => {
   return (
     <footer>
       <div className="social">
-        <TiSocialInstagramCircular size={50} />
-        <TiSocialFacebookCircular size={50} />
-        <TiSocialLinkedinCircular size={50} />
-        <TiSocialGithubCircular size={50} />
-        <AiOutlineWhatsApp size={50} />
+        <a rel="noreferrer" target="_blank" href="https://www.instagram.com/gabriel_hardyfilth/"><TiSocialInstagramCircular size={50} /></a>
+        <a rel="noreferrer" target="_blank" href="https://www.facebook.com/gabrielfraitas"><TiSocialFacebookCircular size={50} /></a>
+        <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/gabriel-freitas-3910061b0/"><TiSocialLinkedinCircular size={50} /></a>
+        <a rel="noreferrer" target="_blank" href="https://github.com/Gabrielhidro"><TiSocialGithubCircular size={50} /></a>
+        <a rel="noreferrer" target="_blank" href="https://api.whatsapp.com/send?phone=5534984109482"><AiOutlineWhatsApp size={50} /></a>
       </div>
       <h3>Created by - Gabriel Freitas da Rocha</h3>
     </footer>
