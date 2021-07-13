25fc820c505fc4ab1e5f9dcfff112f8aaf59c5a8
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-13 01:21:13 -0300

    Create header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../../images/logo.png'
+import './style.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-content">
+        <img className="logo" src={logo} alt="" />
+        <button className="button-lead" type="button">Cadastre-se</button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
