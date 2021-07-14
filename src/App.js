2fdf944c0e80983a692f13abf4fee3acebd16092
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-13 22:27:57 -0300

    Add and configure toastify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+
+import { ToastContainer } from "react-toastify";
 import CardsSection from "./components/CardsSection";
 import Footer from "./components/Footer";
 import FormSection from "./components/FormSection";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
   return (
     <>
+      <ToastContainer/>
       <Header />
       <MainContent />
       <CardsSection />
--- src/components/FormSection/index.js
+++ src/components/FormSection/index.js
@@ -1,56 +1,77 @@
-import React, { useState } from 'react'
-import Animation from '../../images/animation.json'
-import Lottie from 'react-lottie'
-import './style.css'
-
+import React, { useState } from "react";
+import Animation from "../../images/animation.json";
+import Lottie from "react-lottie";
+import { ToastContainer, toast } from "react-toastify";
+import "./style.css";
 
 const FormSection = () => {
-  const [ name, setName ] = useState("");
-  const [ email, setEmail ] = useState("");
-  const [ checked, setChecked ] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [checked, setChecked] = useState(false);
 
-  const defaultOptions  = {
+  const defaultOptions = {
     loop: true,
     autoplay: true,
-    animationData: Animation
-  }
+    animationData: Animation,
+  };
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
-    const dataArray = [name, email]
-    if (checked){
+    const dataArray = [name, email];
+    if (checked) {
       localStorage.setItem("dataKey", JSON.stringify(dataArray));
-      alert('Nome e E-mail salvos no LocalStorage!')
-      return
+      toast.success("Nome e E-mail salvos no LocalStorage!");
     }
-    
-    setName("")
-    setEmail("")
+
+    setName("");
+    setEmail("");
   }
 
   return (
     <section className="form-section">
       <h1>Receba seu cupom de desconto</h1>
       <div className="form-container">
-        <Lottie options={defaultOptions} width={300}/>
+        <Lottie options={defaultOptions} width={300} />
         <form onSubmit={handleSubmit}>
           <h1>Cadastro</h1>
-          <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Nome*" required={true}/>
-          <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder="Email*" required={true}/>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            placeholder="Nome*"
+            required={true}
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            placeholder="Email*"
+            required={true}
+          />
           <p>Termos e condições:</p>
           <div className="form-check">
-            <input value={checked} onChange={() => setChecked(!checked)} type="checkbox" />
-            <label htmlFor="">Aceito receber emails referente as promoções da Gamma-commerce</label>
+            <input
+              value={checked}
+              onChange={() => setChecked(!checked)}
+              type="checkbox"
+            />
+            <label htmlFor="">
+              Aceito receber emails referente as promoções da Gamma-commerce
+            </label>
           </div>
-          {checked ? 
-          (<button id="btn" type="submit" className="btn-form">Cadastrar</button>)
-           : 
-          (<button id="btn" type="submit" className="btn-form" disabled>Cadastrar</button>)
-          }
+          {checked ? (
+            <button id="btn" type="submit" className="btn-form">
+              Cadastrar
+            </button>
+          ) : (
+            <button id="btn" type="submit" className="btn-form" disabled>
+              Cadastrar
+            </button>
+          )}
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default FormSection
+export default FormSection;
